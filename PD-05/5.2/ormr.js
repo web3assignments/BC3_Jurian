e7680a77cd942c908768679f04d2289eb30140a4
Jurian <jurian1998@live.com>
2020-12-06 22:38:59 +0100

    updated ABI

--- PD-05/5.2/ormr.js
+++ PD-05/5.2/ormr.js
@@ -1,134 +1,169 @@
 // Let's use the contract we deployed on the Rinkeby test network!
-// Contract Address: https://rinkeby.etherscan.io/address/0xd0f0d7619cb2fea8f07e82e9a84de76b743bcd67
+// Contract Address: https://rinkeby.etherscan.io/address/0x648a8087721bb3f68d2e9217a55850bfacece905
 
 // Rinkeby / infura provider.
 // const provider = 'https://rinkeby.infura.io/v3/2697eef68b384e938ab43a5499f158ea';
 // const web3 = new Web3(provider);
 
-const contract_address = '0xd0f0d7619cb2fea8f07e82e9a84de76b743bcd67';
+const contract_address = '0x648a8087721bb3f68d2e9217a55850bfacece905';
 
 // Compile your contract in remix, then go to the .JSON artifact and ABI will be there.
 const abi = [
-    {
-        "inputs": [],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": false,
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "name": "encounterResult",
-        "type": "event"
-    },
-    {
-        "inputs": [],
-        "name": "createDragon",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "_name",
-                "type": "string"
-            }
-        ],
-        "name": "createHero",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getDragon",
-        "outputs": [
-            {
-                "components": [
-                    {
-                        "internalType": "string",
-                        "name": "name",
-                        "type": "string"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "id",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "power",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "health",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "goldReward",
-                        "type": "uint256"
-                    }
-                ],
-                "internalType": "struct Dragon",
-                "name": "",
-                "type": "tuple"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getHero",
-        "outputs": [
-            {
-                "components": [
-                    {
-                        "internalType": "string",
-                        "name": "name",
-                        "type": "string"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "power",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "health",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "bank",
-                        "type": "uint256"
-                    }
-                ],
-                "internalType": "struct Hero",
-                "name": "",
-                "type": "tuple"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "startEncounter",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "encounterResult",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "createDragon",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "createHero",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getDragon",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "address",
+						"name": "id",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "power",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "health",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "goldReward",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Dragon",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getHero",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "power",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "health",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "bank",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Hero",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getHeroes",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "power",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "health",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "bank",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Hero[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "startEncounter",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
 ];
 
 var contract;
@@ -160,6 +195,11 @@ function getHero() {
     contract.methods.getHero().call({from: accounts[0]}).then(x => console.log(x));
 }
 
+// Get all heroes in the contract!
+function getHeroes() {
+    contract.methods.getHeroes().call().then(x => console.log(x));
+}
+
 // Create dragon to interact with.
 function createDragon() {
     contract.methods.createDragon().send({from: accounts[0]}).then(x => console.log(x));
@@ -167,10 +207,10 @@ function createDragon() {
 
 // Get our dragon!
 function getDragon() {
-    contract.methods.getDragon().call({from: accounts[0]}).then(x => console.log(x));
+    contract.methods.getDragon().call().then(x => console.log(x));
 }
 
-// Fight!
+// Fight! Has to be async because we have to wait for the contract/blockchain to return something
 async function startEncounter() {
     console.log('initiating dragon eradication procedure!');
     var result = await contract.methods.startEncounter().send({from: accounts[0]});
