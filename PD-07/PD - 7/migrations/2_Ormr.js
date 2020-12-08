b8d129233ad37984410ae7e57777a625cb2795ba
Jurian <jurian1998@live.com>
2020-12-08 17:56:15 +0100

    truffle

---
+++ PD-07/PD - 7/migrations/2_Ormr.js
@@ -0,0 +1,17 @@
+var DragonFactory = artifacts.require("DragonFactory");
+var HeroFactory = artifacts.require("HeroFactory");
+var Ormr = artifacts.require("Ormr");
+
+module.exports = async function(deployer) {
+    await deployer.deploy(DragonFactory);
+    await deployer.deploy(HeroFactory);
+    await deployer.deploy(Ormr);
+
+    DF = await DragonFactory.deployed();
+    HF = await HeroFactory.deployed();
+    Ormr = await Ormr.deployed();
+
+    console.log(`DragonFactory deployed at: ${DF.address}`);
+    console.log(`HeroFactory deployed at: ${HF.address}`);
+    console.log(`Ormr deployed at: ${Ormr.address}`);
+}
