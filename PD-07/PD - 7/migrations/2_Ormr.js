91da06ab8e0405ff729fb8fdc843c38e14aae056
Jurian <jurian1998@live.com>
2020-12-09 00:13:42 +0100

    Update 2_Ormr.js

--- PD-07/PD - 7/migrations/2_Ormr.js
+++ PD-07/PD - 7/migrations/2_Ormr.js
@@ -1,8 +1,10 @@
+var Migrations = artifacts.require("Migrations");
 var DragonFactory = artifacts.require("DragonFactory");
 var HeroFactory = artifacts.require("HeroFactory");
 var Ormr = artifacts.require("Ormr");
 
 module.exports = async function(deployer) {
+    await deployer.deply(Migrations);
     await deployer.deploy(DragonFactory);
     await deployer.deploy(HeroFactory);
     await deployer.deploy(Ormr);
@@ -11,6 +13,7 @@ module.exports = async function(deployer) {
     HF = await HeroFactory.deployed();
     Ormr = await Ormr.deployed();
 
+    console.log(`Migrations deployed at: ${Migrations.address}`);
     console.log(`DragonFactory deployed at: ${DF.address}`);
     console.log(`HeroFactory deployed at: ${HF.address}`);
     console.log(`Ormr deployed at: ${Ormr.address}`);
